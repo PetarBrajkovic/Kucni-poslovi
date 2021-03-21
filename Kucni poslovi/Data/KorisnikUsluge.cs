aa68504b5ea45b9a18b5ffa5ade28fe325d69e9a
Petar Brajković <petar.brajkovic@elfak.rs>
2021-03-22 00:20:00 +0100

    Add project files.

---
+++ Kucni poslovi/Data/KorisnikUsluge.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kucni_poslovi.Data
+{
+    
+    public class KorisnikUsluge : RegistrovaniKorisnik
+    {
+        public List<Oglas> MojiOglasi { get; set; }
+
+        public override void OceniOglas(int ocena)
+        {
+            int count = 0;
+            foreach (var oglas in MojiOglasi)
+            {
+                if (oglas.Stanje == Stanje.Izvršen || oglas.Stanje == Stanje.Neaktivan)
+                {
+                    if (oglas.OcenjenKorisnik != null)
+                        count++;
+                }
+            }
+            ProsecnaOcena = ((count-1) * ProsecnaOcena + ocena) / count;
+        }
+
+
+        public KorisnikUsluge()
+        {
+            MojiOglasi = new List<Oglas>();
+        }
+    }
+}
---
+++ Kucni poslovi/Data/PruzalacUsluge.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kucni_poslovi.Data
+{
+ 
+    public class PruzalacUsluge : RegistrovaniKorisnik
+    {
+
+        public TipUsluge TipUsluga { get; set; }
+        public List<Oglas> PreuzetOglas { get; set; }
+        public List<Zahtev> MojiZahtevi { get; set; }
+        public List<EvidencijaOglasa> Evidencije { get; set; }
+
+
+        public override void OceniOglas(int ocena)
+        {
+            int count = 0;
+            foreach (var oglas in PreuzetOglas)
+            {
+                if (oglas.Stanje == Stanje.Izvršen || oglas.Stanje == Stanje.Neaktivan)
+                {
+                    if (oglas.OcenjenPruzalac != null)
+                        count++;
+                }
+            }
+            ProsecnaOcena = ((count-1) * ProsecnaOcena + ocena) / count;
+        }
+    }
+}
